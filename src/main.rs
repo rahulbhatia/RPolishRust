6f79a7e3d885cc3f4697987e6bc5dc75085c3084
rbhatia5 <rahul.bhatia.k@gmail.com>
2018-02-17 01:17:38 -0500

    uses reverse polish notation correctly

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 use std::io;
 
 fn main() {
-    let mut v: Vec<f64> = Vec::new();
+    
     loop {
         print!(">> ");
         io::Write::flush(&mut io::stdout()).expect("flush failed!");
@@ -13,8 +13,9 @@ fn main() {
             Ok(_n) => {
                 //println!("{} bytes read", n);
                 //println!("{}", input);
-                tokenize(&mut input, &mut v);
-                println!("Stack: {:?}", v);
+                let answer = process(&mut input);
+                println!("{}", answer);
+
             }
             Err(error) => println!("error: {}", error),
         }
@@ -45,22 +46,25 @@ fn divide(v: &mut Vec<f64>) {
     v.push(b / a);
 }
 
-fn tokenize(input: &mut String, v: &mut Vec<f64>) {
-    
+fn process(input: &mut String) -> f64 {
+    let mut v: Vec<f64> = Vec::new();
+    // Drop the newline
     input.pop();
+
     for s in input.split(" ") {
         match s.as_ref() {
-            "+" => add(v),
-            "-" => subtract(v),
-            "*" => multiply(v),
-            "/" => divide(v),
+            "+" => add(&mut v),
+            "-" => subtract(&mut v),
+            "*" => multiply(&mut v),
+            "/" => divide(&mut v),
+            " " => (),
             _ => match s.parse::<f64>() {
                 Ok(n) => v.push(n),
                 Err(e) => {
-                    println!("{}: {}", e, s);
+                    //println!("{}: '{}'", e, s);
                 }
             }
         }
     }
-
+    v.pop().unwrap()
 }
